eb2375f286caf38984759dc2ccd642ecea4a188b
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2018-04-22 23:11:55 +1000

    added more information about the packed struct etc...

--- base64_decode/base64_decode.py
+++ base64_decode/base64_decode.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import binascii
+import struct
 
 #binstr=b"thisisunreadablebytes"
 
@@ -16,4 +17,9 @@ decoded=binascii.a2b_base64(encoded)
 ba=bytearray(decoded)
 print("%s, Length is: %d" %(list(ba), len(ba)))
 
+print "Printing Little Endian " %(struct.pack('>I',ba[0:4]))
+print "Printing Big Endian " %(struct.pack('>I',ba[0:4]))
+print struct.unpack('4B', ba[0:4])
 # print binascii.b2a_hex(binstr)
+
+#def lt_2_big_end ( arg, 
