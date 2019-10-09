325075c9e2b4fe253e415b790cf69300a156cc13
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2019-10-09 19:54:45 +0200

    Minor changes to ptr of array datatype and arry of pointers

--- general_c_testing/ptr.c
+++ general_c_testing/ptr.c
@@ -5,18 +5,24 @@ void arraysVsPtrs(void)
     const int NOENTRIES = 100;
     int c[NOENTRIES];
     unsigned int (*arrptr)[10];
+    unsigned int *(ptrarr[10]);
     unsigned int *(ap[NOENTRIES]);
     arrptr = (unsigned int(*)[10]) c;
+    
+    int count = 0;
 
     for ( int i=0;i<NOENTRIES;i++)
     {
         c[i] = i;
         ap[i] = c + i;
+        if(i%10 == 0)
+            ptrarr[i/10] = &c[i/10];;
     }
 
     for (int i=0;i<10;i++)
     {
-        printf("Arrptr + %d points to %u\n",i,**(arrptr++));
+        printf("Arrptr + %d points to %u\n",i,*(arrptr[i]));
+        printf("Ptrarr + %d points to %u\n",i,*(ptrarr[i]));
         printf("ap[%d] is %d\n",i+10,*ap[i+10]);
     }
     return;
@@ -27,6 +33,7 @@ void const_ptrs(void)
     const char c1 = 'a',c2 = 'b';
     char * const h = &c1;
     printf("c1=%c c2=%c &c1=%x &c2=%x *h=%c h=%x\n",c1,c2,&c1,&c2,*h,h);
+    // Below causes error
     //h = &c2;
     *h = 'c';
     printf("c1=%c c2=%c &c1=%x &c2=%x *h=%c h=%x\n",c1,c2,&c1,&c2,*h,h);
