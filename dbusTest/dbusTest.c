7e67dc73c761126d274bb49ce519a3e3e254c884
Karthik Sukumar <karthik.n.sukumar@gmail.com>
2021-06-07 12:23:43 +0000

    Dbus Name changed

--- dbusTest/dbusTest.c
+++ dbusTest/dbusTest.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include <dbus/dbus.h>
+//#include <dbus/dbus-glib.h>
 
 int main(void)
 {
@@ -7,10 +8,17 @@ int main(void)
     DBusConnection *conn;
     dbus_error_init(&error);
     conn = dbus_bus_get(DBUS_BUS_SYSTEM, &error);
+    dbus_connection_set_exit_on_disconnect(conn, TRUE);
     if(!conn)
     {
         fprintf(stderr, "%s: %s\n", error.name, error.message);
         return 1;
     }
+    printf("%s\n", dbus_bus_get_unique_name(conn));
+    dbus_bus_set_unique_name(conn, "MyDBusConnection");
+    printf("%s\n", dbus_bus_get_unique_name(conn));
+
+    //dbus_connection_close(conn);
+
     return 0;
 }
